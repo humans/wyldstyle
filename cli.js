68d352691542c325e8a59052e08aae4e487d5d0f
Jaggy Gauran <i.am@jag.gy>
2016-05-18 18:59:57 +0800

    Build the utility classes from emmet.

--- cli.js
+++ cli.js
@@ -31,11 +31,53 @@ function cache_utiltiies(cache, data) {
     return array_merge_distinct(cache, matches);
 }
 
+function expand_css(emmet) {
+    let utility = emmet.replace('u-', '').trim();
+    let [ attribute, value ] = utility.split(':');
+
+    let attributes = {
+        'fw': 'font-weight',
+        'fs': 'font-size',
+
+        'p': 'padding',
+        'pl': 'padding-left',
+        'pr': 'padding-right',
+        'pt': 'padding-top',
+        'pb': 'padding-bottom',
+
+        'm': 'margin',
+        'ml': 'margin-left',
+        'mr': 'margin-right',
+        'mt': 'margin-top',
+        'mb': 'margin-bottom',
+    };
+
+    return `${attributes[attribute]}: rem(${value});`;
+}
+
+function build_css(classes) {
+    let css = "";
+
+    for (index in classes) {
+        let emmet    = classes[index];
+        let escaped  = emmet.replace(':', '\\:').replace('@', '\\@');
+        let selector = `.${escaped}`;
+        let expanded = expand_css(emmet);
+
+        css = css + `${selector} { ${expanded} }\n`;
+    }
+
+    return css;
+}
+
 function process_files(event, path) {
     console.log(event, path);
 
     fs.readFile(path, 'utf8', (error, data) => {
-        cache = cache_utiltiies(cache, data);
+        cache = cache_utiltiies(cache, data).sort();
+
+        let stylesheets = build_css(cache);
+        console.log(stylesheets);
 
         console.log('Utilities: ', cache);
     });
