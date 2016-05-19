bc55fd519feb75e03206c1e95767461d2c66365e
Jaggy Gauran <i.am@jag.gy>
2016-05-19 12:29:28 +0800

    Extract the main watcher into it's own module.

--- cli.js
+++ cli.js
@@ -1,41 +1,12 @@
-let filesystem        = require('fs');
-let chokidar          = require('chokidar');
-let array_unique      = require('./src/array_unique');
-let extract_utilities = require('./src/extract_utilities');
-let generate_css      = require('./src/generate_css');
-let watch_directory   = process.argv[2];
-let output_file       = process.argv[3];
+let lucy = require('./src/lucy');
 
 if (process.argv.length != 4) {
+    console.log('Usage: lucy <directory> <output> [--config=<lucy.json>]');
+
     process.exit();
 }
 
-// Ignore the dotfiles.
-let config = {
-    ignored: /[\/\\]\./,
-}
-
-let cache = [];
-
-chokidar.watch(watch_directory, config).on('all', (event, filename) => {
-    console.log(event, filename);
-
-    filesystem.readFile(filename, 'utf8', (error, data) => {
-        if (error) {
-            return;
-        }
-
-        let utilities = extract_utilities(data);
-        let styles = generate_css(utilities);
-
-        cache = array_unique(cache.concat(styles)).sort();
-
-        filesystem.writeFile(output_file, cache.join("\n"), (error) => {
-            if (error) {
-                return console.log(err);
-            }
-
-            console.log(`File saved on ${__dirname}/${output_file}`);
-        });
-    });
+lucy({
+    directory: process.argv[2],
+    output: process.argv[3],
 });
---
+++ src/lucy.js
@@ -0,0 +1,30 @@
+let filesystem        = require('fs');
+let chokidar          = require('chokidar');
+let array_unique      = require('./array_unique');
+let extract_utilities = require('./extract_utilities');
+let generate_css      = require('./generate_css');
+
+// Ignore the dotfiles.
+let config = { ignored: /[\/\\]\./, }
+let cache  = [];
+
+module.exports = function (options) {
+    chokidar.watch(options.directory, config).on('all', (event, filename) => {
+        console.log(event, filename);
+
+        filesystem.readFile(filename, 'utf8', (error, data) => {
+            if (error) { return; }
+
+            let utilities = extract_utilities(data);
+            let styles    = generate_css(utilities);
+
+            cache = array_unique(cache.concat(styles)).sort();
+
+            filesystem.writeFile(options.output, cache.join("\n"), (error) => {
+                if (error) { return console.log(err); }
+
+                console.log(`File saved on ${__dirname}`);
+            });
+        });
+    });
+}
