5e937b2b56293e66b8027f33f0fe2d5f173ca1df
Jaggy Gauran <i.am@jag.gy>
2016-05-25 22:51:26 +0800

    Extract out the wyldstyle to the watcher and actually look for a config file.

--- index.js
+++ index.js
@@ -1,32 +1,36 @@
 let filesystem = require('fs');
-let wyldstyle = require('./src/wyldstyle');
-let read_config = require('./src/read_config');
-let config = {};
-let has_config = true;
+let Watcher = require('./src/Watcher');
+let Config = require('./src/Config');
 
+let usage = 'Usage: wyldstyle <directory> <output> [--config=<wyldstyle.json>]';
+let fileConfig = {};
+let hasConfig = false;
+let cliConfig = { // Config from the cli argmuments
+    directory: process.argv.slice(2, -1),
+    output: process.argv.slice(-1)[0],
+};
+
+// Let's see if the file exists.
+//
+// accessSync seems to throw an exception when the file does not exist sooo,
+// that pretty much sucks
 try {
-    let path = './wyldstyle.json';
+    let contents;
+    let path = `${process.cwd()}/wyldstyle.json`;
 
-    filesystem.accessSync(path, fs.F_OK)
+    filesystem.accessSync(path, fs.F_OK);
 
-    config = read_config(path);
-} catch (e) {
-    has_config = false;
-}
+    hasConfig = true;
+    fileConfig = JSON.parse(filesystem.readFileSync(path, 'utf8'));
+} catch (error) {}
 
-
-if (! has_config  && process.argv.length < 4) {
-    console.log('Usage: wyldstyle <directory> <output> [--config=<wyldstyle.json>]');
+if (! hasConfig && process.argv.length < 4) {
+    console.log(usage);
 
     process.exit();
 }
 
-if (! has_config) {
-    config = {
-        directory: process.argv.slice(2, -1),
-        output: process.argv.slice(-1)[0],
-    }
-}
+let config = new Config(hasConfig ? fileConfig : cliConfig);
+let watcher = new Watcher(config.all());
 
-
-wyldstyle(config);
+watcher.start();
